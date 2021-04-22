983531122432178280558c26effc3e52a3a60937
Shyrz <i@shyrz.ch>
2021-04-22 11:18:36 +0800

    Initial commit

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e3b75e123cbb47b78786c0ba5473295c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Roam Reality',
+  domain: 'shyrz.me',
+  author: 'Shyrz',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Life, the Universe and Everything.',
+  socialImageTitle: 'Shyrz',
+  socialImageSubtitle: 'Life, the Universe and Everything.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'shyrz__',
+  github: 'shyrz',
+  linkedin: 'shyrz',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
